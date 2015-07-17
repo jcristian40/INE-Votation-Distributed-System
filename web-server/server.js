4dda6919f4c6141b6f890cabfb9c9c2a000afa34
sguerra <sguerravega@outlook.com>
2015-07-17 14:42:44 -0500

    WS: Simple socket.io interaction on vote received

--- web-server/server.js
+++ web-server/server.js
@@ -10,6 +10,11 @@ var servers = {
 	}
 }
 
+var votes = [];
+var status = {
+	total : 0
+};
+
 // UDP Server
 
 var dgram = require("dgram");
@@ -28,6 +33,11 @@ udpServer.on("listening", function(){
 udpServer.on("message", function(msg, rinfo){
 	console.log("message: ", msg, rinfo.address, ":", rinfo.port);
 
+	votes.push({id: votes.length});
+
+	status.total++;
+	io.emit('vote', status);
+	
 	var res = new Buffer('OK');
 	var client = dgram.createSocket("udp4");
 	client.send(res, 0, res.length, rinfo.port, rinfo.host, function(err, bytes){
@@ -43,9 +53,29 @@ udpServer.bind(servers.udp.port, servers.udp.host);
 var http = require('http');
 
 var httpServer = http.createServer(function(req, res){
-	res.writeHead(200, 'Content-Type: text/json');
-	res.end('{}');
+	res.writeHead(200, 'Content-Type: text/html');
+
+	var html = '<!DOCTYPE html>';
+	html += '<html>';
+	html += '	<head>';
+	html += '		<meta charset="UTF-8">';
+	html += '		<script src="https://cdn.socket.io/socket.io-1.3.6.js"></script>';
+	html += '		<script type="text/javascript">';
+	html += '			var socket = io("http://localhost:8080");';
+	html += '			socket.on("vote", function(data){';
+	html += '				document.getElementById("content").innerHTML = JSON.stringify(data);';
+	html += '			});';
+	html += '		</script>';
+	html += '	</head>';
+	html += '	<body>';
+	html += '		<div id="content">'+JSON.stringify(status)+'</div>';
+	html += '	</body>';
+	html += '</html>';
+
+	res.end(html);
 });
+
+var io = require('socket.io')(httpServer);
 
 httpServer = httpServer.listen(servers.http.port, servers.http.host);
 
