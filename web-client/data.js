306ee0e62829e46c635ed6db9c6b6ed53083cfa6
sguerra <sguerravega@outlook.com>
2015-07-18 02:52:41 -0500

    WC: votation graph and vote query implemented with socket.io

---
+++ web-client/data.js
@@ -0,0 +1,69 @@
+
+var PARTIES_DATA = [
+    {
+        key : 'PRI',
+        name : 'PRI',
+        color : 'white',
+        image : 'PRI.png'
+    },
+    {
+        key : 'PAN',
+        name : 'PAN',
+        color : 'rgb(79, 123, 255)',
+        image : 'PAN.png'
+    },
+    {
+        key : 'PRD',
+        name : 'PRD',
+        color : 'rgb(249, 225, 66)',
+        image : 'PRD.png'
+    },
+    {
+        key : 'P_T',
+        name : 'PT',
+        color : 'rgb(255, 54, 54)',
+        image : 'P_T.png'
+    },
+    {
+        key : 'VDE',
+        name : 'Partido Verde',
+        color : 'rgb(68, 194, 77)',
+        image : 'VDE.png'
+    },
+    {
+        key : 'MVC',
+        name : 'Movimiento Ciudadano',
+        color : 'rgb(255, 163, 55)',
+        image : 'MVC.png'
+    },
+    {
+        key : 'NVA',
+        name : 'Nueva Alianza',
+        color : 'rgb(80, 233, 219)',
+        image : 'NVA.png'
+    },
+    {
+        key : 'MOR',
+        name : 'MORENA',
+        color : 'rgb(185, 105, 70)',
+        image : 'MOR.jpg'
+    },
+    {
+        key : 'HUM',
+        name : 'Partido Humanista',
+        color : 'rgb(195, 128, 238)',
+        image : 'HUM.png'
+    },
+    {
+        key : 'ENC',
+        name : 'Encuentro Ciudadano',
+        color : 'rgb(227, 74, 111)',
+        image : 'ENC.jpg'
+    },
+    {
+        key : 'FRD',
+        name : 'Fraude',
+        color : '#ccc',
+        image : 'RFD.jpg'
+    }
+];
--- web-client/index.js
+++ web-client/index.js
@@ -1,3 +1,143 @@
+var APP = {
+    positionList : null,
+    chart : null,
+    socketURL : 'http://192.168.0.7:8080',
+    httpURL : 'http://192.168.0.7:8081',
+    socket : null,
+    parties : PARTIES_DATA
+}
+
+APP.init = function () {
+
+    APP.socket = io(APP.socketURL);
+    APP.positionList = document.querySelector('#positions');
+    APP.chartElement = document.querySelector('#chart-container figure');
+
+    APP.searchForm = document.querySelector('#search form');
+    APP.txtCURP = document.querySelector('#txt-curp');
+    APP.txtPhone = document.querySelector('#txt-phone');
+
+    APP.serchResult = document.querySelector('#search-result');
+
+    APP.chartElement = document.querySelector('#chart-container figure');
+
+    APP.searchForm.addEventListener('submit', APP.onSearch);
+
+    APP.parties = APP.parties.map(function (item) {
+        return new Party(item);
+    });
+
+    APP.chart = new PieChart();
+
+    APP.socket.on('update', function (data) {
+        APP.updateParties(data.votes);
+        APP.chart.update();
+    });
+}
+
+APP.updateParties = function(votes) {
+
+    APP.parties.forEach(function(party){
+        party.votes = votes[party.key];
+    });
+
+    APP.parties.sort(function (a, b) {
+
+        var value = (a.votes - b.votes) * -1;
+
+        if(value === 0){
+            return a.name > b.name;
+        }
+
+        return value;
+    });
+
+    var elements = APP.parties.map(function (party, index) {
+        party.position = index + 1;
+        return party.getHTML();
+    });
+
+    APP.positionList.innerHTML = elements.join('');
+}
+
+APP.onSearch = function (e) {
+    e.preventDefault();
+
+    var url = APP.httpURL + '/votes';
+    url += '?curp=' + APP.txtCURP.value;
+    url += '&phone=' + APP.txtPhone.value;
+
+    fetch(url)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (vote) {
+
+        return APP.showSearchResult(vote);
+
+    })
+    .catch(function (err) {
+        console.log(err)
+        APP.showSearchResult();
+    });
+
+}
+
+APP.showSearchResult = function (vote) {
+
+    var html = ' \
+        <div id="no-result"> \
+            No se encontró ningún resultado \
+        </div> \
+    ';
+
+    if(vote){
+
+        var results = APP.parties.filter(function(party) {
+            return party.key === vote.party;
+        });
+
+        if(results.length === 1){
+
+            var party = results[0];
+
+            var partyStyles = '';
+
+            if(party.key !== 'FRD'){
+                partyStyles = 'style="background-image: url(\'./logos/' + party.image + '\')"';
+            }
+
+            html = ' \
+                <div \
+                    style="background-image: url(\'https://robohash.org/' + party.key + '.png?set=set2\')" \
+                    class="profile-icon"> \
+                    <div class="party-icon-mini" ' + partyStyles + '></div> \
+                </div> \
+                <div class="profile-name"> \
+                    <p> \
+                        <strong>Partido:</strong> \
+                        <span id="party-name">' + party.name + '</span> \
+                    </p> \
+                    <p> \
+                        <strong>CURP:</strong> \
+                        <span id="curp">' + vote.curp + '</span> \
+                    </p> \
+                    <p> \
+                        <strong>Teléfono:</strong> \
+                        <span id="phone">' + vote.phone + '</span> \
+                    </p> \
+                </div> \
+            ';
+
+        }
+
+
+    }
+
+    APP.serchResult.innerHTML = html;
+
+    return true;
+}
 
 var SAMPLE_DATA = {
     votes : {
@@ -10,74 +150,13 @@ var SAMPLE_DATA = {
         NVA : 1,
         MOR : 1,
         HUM : 1,
-        ENC : 1
-    },
-    total : 7
+        ENC : 1,
+        FRD : 1
+    }
 }
 
 
-var parties = [
-    {
-        key : 'PRI',
-        name : 'PRI',
-        color : '',
-        image : 'PRI.png'
-    },
-    {
-        key : 'PAN',
-        name : 'PAN',
-        color : '',
-        image : 'PAN.png'
-    },
-    {
-        key : 'PRD',
-        name : 'PRD',
-        color : '',
-        image : 'PRD.png'
-    },
-    {
-        key : 'P_T',
-        name : 'PT',
-        color : '',
-        image : 'P_T.png'
-    },
-    {
-        key : 'VDE',
-        name : 'Partido Verde',
-        color : '',
-        image : 'VDE.png'
-    },
-    {
-        key : 'MVC',
-        name : 'Movimiento Ciudadano',
-        color : '',
-        image : 'MVC.png'
-    },
-    {
-        key : 'NVA',
-        name : 'Nueva Alianza',
-        color : '',
-        image : 'NVA.png'
-    },
-    {
-        key : 'MOR',
-        name : 'MORENA',
-        color : '',
-        image : 'MOR.jpg'
-    },
-    {
-        key : 'HUM',
-        name : 'Partido Humanista',
-        color : '',
-        image : 'HUM.png'
-    },
-    {
-        key : 'ENC',
-        name : 'Encuentro Ciudadano',
-        color : '',
-        image : 'ENC.jpg'
-    },
-];
+
 
 var Party = function (data) {
     this.key = data.key;
@@ -90,50 +169,71 @@ var Party = function (data) {
 
 Party.prototype.getHTML = function () {
 
+    var partyStyles = '';
+
+    if(this.key !== 'FRD'){
+        partyStyles = 'style="background-image: url(\'./logos/' + this.image + '\')"';
+    }
+
     return '\
         <li class="party"> \
             <div \
                 style="background-image: url(\'https://robohash.org/' + this.key + '.png?set=set2\')" \
                 class="profile-icon" data-position="' + this.position + '"> \
-                <div \
-                    style="background-image: url(\'./logos/' + this.image + '\')" \
-                    class="party-icon-mini"></div> \
+                <div class="party-icon-mini" ' + partyStyles + '></div> \
             </div> \
             <div class="profile-name">70,000,000</div> \
-            <div class="profile-votes">70,000,000</div> \
+            <div class="profile-votes">' + this.votes + '</div> \
         </li> \
     '
 }
 
+var PieChart = function () {
 
-function updateParties(votes) {
-
-    parties.forEach(function(party){
-        party.votes = votes[party.key];
+    this.chart = new Chartist.Pie(APP.chartElement, {
+        series : []
     });
 
-    parties.sort('votes');
+    this.chart.on('draw', this.onDraw);
 
-    var elements = parties.map(function (party, index) {
-        party.position = index + 1;
-        return party.getHTML();
-    });
-
-    $POSITIONS_LIST.innerHTML = elements.join('');
 }
 
+PieChart.prototype.sum = function(a, b) {
+    return a + b.value
+};
 
 
-var $POSITIONS_LIST = null;
+PieChart.prototype.onDraw = function(context) {
 
-document.addEventListener("DOMContentLoaded", function(event) {
+    if(context.type === 'slice') {
 
-    $POSITIONS_LIST = document.querySelector('#positions');
+        context.element.attr({
+            style: 'fill:' + context.meta
+        });
+    }
 
-    parties = parties.map(function (item) {
-        return new Party(item);
+}
+
+PieChart.prototype.update = function() {
+
+    var series = APP.parties.map(function (party) {
+        return {
+            value : party.votes,
+            meta : party.color
+        };
     });
 
-    updateParties(SAMPLE_DATA.votes);
+    var that = this;
 
-});
+    this.chart.update({
+        series : series
+    },{
+        labelInterpolationFnc : function(value) {
+            return Math.round(value / series.reduce(that.sum, 0) * 100) + '%';
+        }
+    });
+
+};
+
+
+document.addEventListener('DOMContentLoaded', APP.init);
